9a14b5668b8af7e786f8aa51d2e65cb6d3d705d8
Pablo Heredia <pheredia2004@gmail.com>
2024-08-27 22:51:29 -0600

    Añadida vision computacional y cambiado tamaño de grid

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, WebSocket
 from model.warehouse_model import WarehouseModel
 import numpy as np
+from vision.vision import ComputerVision
 
 app = FastAPI()
 
@@ -17,8 +18,8 @@ async def websocket_endpoint(websocket: WebSocket):
         'R': 5,      # Número de robots
         'K': 20,     # Número de cajas
         'P': 4,      # Número de pilas
-        'M': 10,     # Altura de la cuadrícula
-        'N': 10,     # Ancho de la cuadrícula
+        'M': 11,     # Altura de la cuadrícula
+        'N': 11,     # Ancho de la cuadrícula
         'steps': 500 # Número de pasos
     }
     model = WarehouseModel(parameters)
@@ -65,6 +66,23 @@ async def websocket_endpoint(websocket: WebSocket):
             
             #Regresa los pasos y la información de posicion de los agentes
             await websocket.send_json({"status": "step completed", 'step': model.t, "state": agents_data})
+        elif data == "close":
+            await websocket.close()
+            break
+
+@app.websocket("/ws/vision/")
+async def websocket_endpoint_frame(websocket: WebSocket):
+    await websocket.accept()
+
+    
+    while True:
+        data = await websocket.receive_text()  # Espera mensaje del cliente
+        if data:
+         
+            vision = ComputerVision(data)
+
+            print(vision)
+            await websocket.send_json({"status": "step completed"})
         elif data == "close":
             await websocket.close()
             break
---
+++ vision/vision.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+
+def ComputerVision(encoded_image):
+  # Load environment variables from the .env file
+  load_dotenv()
+
+  # Access the API key
+  api_key = os.getenv('OPENAI_API_KEY')
+
+  # Getting the base64 string
+  base64_image = encoded_image
+
+  headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+  }
+
+  payload = {
+    "model": "gpt-4o-mini",
+    "messages": [
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": "You are a robot in a warehouse and your goal is to detect what you are seeing. There are 4 possible options: A box, a dron, a pile of boxes or dron carrying a box. There can be multiple things in the same image like a robot a box and a dron carrying a box, etc. Identify what you are seeing and the image and return a text saying 'Im seeing x'."
+          },
+          {
+            "type": "image_url",
+            "image_url": {
+              "url": f"data:image/jpeg;base64,{base64_image}"
+            }
+          }
+        ]
+      }
+    ],
+    "max_tokens": 300
+  }
+
+  response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+  return response.json()
